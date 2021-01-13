6633a062649a391092222f4203baa5d943c400c5
Eric Hippler <hippler.eric@gmail.com>
2021-01-12 19:47:21 -0500

    outlook email automation complete

--- bidlist_outlook.py
+++ bidlist_outlook.py
@@ -1,4 +1,23 @@
-
+import win32com.client as client
+from sfcbidlist import bid_list, tasksJSON
+from print_bid_list import printHTML
 
 # MS Outlook
 # https://www.youtube.com/watch?v=ZvmFHwAjXHI&feature=youtu.be
+
+def main():
+    
+    # Get HTML formatted tasks for email body
+    tasks = bid_list()
+    json_list = tasksJSON(tasks)
+    html_body = printHTML(json_list.tasks)
+    
+    # Launch outlook email
+    outlook = client.Dispatch("Outlook.Application")
+    message = outlook.CreateItem(0)
+    message.Display()
+    message.Subject = "Upcoming Bids - "
+    message.HTMLBody = html_body
+
+if __name__ == "__main__":
+    main()
